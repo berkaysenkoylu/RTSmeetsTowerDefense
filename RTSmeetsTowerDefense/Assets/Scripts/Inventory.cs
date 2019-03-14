ad22be393ad0720025ea43cacc8aef92b425bc01
frobenius90 <senkoylu_berkay@yahoo.com>
2019-03-14 19:32:49 +0300

    Added a basic player inventory

---
+++ RTSmeetsTowerDefense/Assets/Scripts/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class Inventory
+{
+    public enum ResourceTypes { log, rock }
+
+    Dictionary<ResourceTypes, int> inventoryContent = new Dictionary<ResourceTypes, int>();
+
+    public Inventory()
+    {
+        foreach (ResourceTypes resType in Enum.GetValues(typeof(ResourceTypes)))
+        {
+            inventoryContent.Add(resType, 0);
+        }
+    }
+
+    public Dictionary<ResourceTypes, int> GetInventoryContent()
+    {
+        return inventoryContent;
+    }
+
+    public void AddItemToInventory(ResourceTypes type, int amount)
+    {
+        inventoryContent[type] += amount;
+    }
+}
---
+++ RTSmeetsTowerDefense/Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    Inventory playerInventory;
+
+    void Start()
+    {
+        playerInventory = new Inventory();
+
+        foreach(KeyValuePair<Inventory.ResourceTypes, int> item in playerInventory.GetInventoryContent())
+        {
+            Debug.Log(item.Key + ": " + item.Value);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
