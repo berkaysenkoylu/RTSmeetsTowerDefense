de85609c38e54db154b1417a054988c3e2cd796a
berkaysenkoylu <senkoylu_berkay@yahoo.com>
2019-08-02 23:23:47 +0300

    Added zombie behaviour tree (incomplete)

---
+++ RTSmeetsTowerDefense/Assets/Scripts/AI/ZombieAI.cs
@@ -0,0 +1,244 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourTree;
+
+public class ZombieAI : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public Node CreateBehaviorTree(Zombie zombie, GameObject mainTarget, LayerMask _mask)
+    {
+        Selector deathSequence = new Selector("Death Sequence", new ShouldIDie(zombie), new Die(zombie));
+
+        Sequence targetSelectionSequence = new Sequence("Do I have a target?", new HaveTarget(zombie, _mask, zombie.GetMainTarget()), new SetDestination(zombie));
+
+        Sequence moveSequence = new Sequence("Move Sequence", new Inverter(new CheckDistance(zombie)), new MoveTowardsEnemy(zombie));
+
+        Sequence attackSequence = new Sequence("Attack Sequence", new ReadyToAttack(zombie));
+
+        Sequence treeRoot = new Sequence("Root behaviour", deathSequence, targetSelectionSequence, moveSequence, attackSequence);
+
+        return treeRoot;
+    }
+
+    // ================= LEAF Nodes ================ //
+    public class ShouldIDie : Leaf
+    {
+        Zombie self;
+
+        public ShouldIDie(Zombie own)
+        {
+            self = own;
+        }
+
+        public override NodeStatus OnBehave()
+        {
+            if(self.getHealthPoint() <= 0f)
+            {
+                return NodeStatus.FAILURE;
+            }
+            
+            return NodeStatus.SUCCESS;
+        }
+
+        public override void OnReset() { }
+    }
+
+    public class Die : Leaf
+    {
+        Zombie self;
+
+        public Die(Zombie own)
+        {
+            self = own;
+        }
+
+        public override NodeStatus OnBehave()
+        {
+            // Zombie dies
+            // TODO: Implement death mechanism: To destroy, to deactivate, or ... something else?
+            Debug.Log("Zomboi is dead");
+            Destroy(self.gameObject);
+
+            // To break the whole sequence
+            return NodeStatus.FAILURE;
+        }
+
+        public override void OnReset() { }
+    }
+
+    public class HaveTarget : Leaf
+    {
+        Zombie self;
+        GameObject target;
+        LayerMask attackMask;
+
+        public HaveTarget(Zombie own, LayerMask _mask, GameObject defaultTarget)
+        {
+            self = own;
+            attackMask = _mask;
+            target = defaultTarget;
+        }
+
+        public override NodeStatus OnBehave()
+        {
+            // Check if there are potential targets in the zombie's surroundings (such as player, towers, ...etc)
+            Collider[] targetColliders = Physics.OverlapSphere(self.gameObject.transform.position, 10f, attackMask);
+
+            GameObject closestTarget = GetClosestTarget(targetColliders);
+
+            if(closestTarget != null)
+            {
+                target = closestTarget;
+            }
+
+            if (target != null)
+            {
+                self.SetMainTarget(target);
+
+                return NodeStatus.SUCCESS;
+            }
+            else
+            {
+                return NodeStatus.FAILURE;
+            }
+        }
+
+        GameObject GetClosestTarget(Collider[] potentialTargets)
+        {
+            float distance = 999f;
+            GameObject closestTarget = null;
+
+            foreach(Collider potentialTarget in potentialTargets)
+            {
+                float currDistance = Vector3.Distance(self.transform.position, potentialTarget.gameObject.transform.position);
+
+                if (currDistance < distance)
+                {
+                    closestTarget = potentialTarget.gameObject;
+                    distance = currDistance;
+                }
+            }
+
+            return closestTarget;
+        }
+
+        public override void OnReset() { }
+    }
+
+    public class SetDestination : Leaf
+    {
+        Zombie self;
+
+        public SetDestination(Zombie own)
+        {
+            self = own;
+        }
+
+        public override NodeStatus OnBehave()
+        {
+            Debug.Log("Setting destination");
+
+            GameObject target = self.GetMainTarget();
+
+            if (target != null)
+            {
+                self.SetTargetPosition(target.transform.position);
+
+                return NodeStatus.SUCCESS;
+            }
+
+            return NodeStatus.FAILURE;
+        }
+
+        public override void OnReset() { }
+    }
+
+    // TODO: Stuff that is below is likely to be changed...
+
+    public class CheckDistance : Leaf
+    {
+        Zombie self;
+
+        public CheckDistance(Zombie own)
+        {
+            self = own;
+        }
+
+        public override NodeStatus OnBehave()
+        {
+            if (self.TargetWithinDistance())
+            {
+                Debug.Log("Target is within distance");
+                return NodeStatus.SUCCESS;
+            }
+            else
+            {
+                Debug.Log("Target is not within distance");
+                return NodeStatus.FAILURE;
+            }
+        }
+
+        public override void OnReset() { }
+    }
+
+    public class MoveTowardsEnemy : Leaf
+    {
+        Zombie self;
+
+        public MoveTowardsEnemy(Zombie own)
+        {
+            self = own;
+        }
+
+        public override NodeStatus OnBehave()
+        {
+            float distance = Vector3.Distance(self.gameObject.transform.position, self.GetMainTarget().transform.position);
+
+            if (distance > 4f)
+            {
+                self.MoveToDestination();
+            }
+
+            if(distance <= 4f)
+            {
+                return NodeStatus.SUCCESS;
+            }
+            else
+            {
+                return NodeStatus.RUNNING;
+            }
+        }
+
+        public override void OnReset() { }
+    }
+
+    public class ReadyToAttack : Leaf
+    {
+        Zombie self;
+
+        public ReadyToAttack(Zombie own)
+        {
+            self = own;
+        }
+
+        public override NodeStatus OnBehave()
+        {
+            Debug.Log("Zombie is ready to attack");
+
+            return NodeStatus.SUCCESS;
+        }
+
+        public override void OnReset() { }
+    }
+    // ============================================= //
+}
---
+++ RTSmeetsTowerDefense/Assets/Scripts/Zombie.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using BehaviourTree;
+
+public class Zombie : MonoBehaviour
+{
+    public float healthPoint = 100f;
+    public float attackRange = 4f;
+    public GameObject mainTarget;
+    public LayerMask _mask;
+
+    [SerializeField]
+    Vector3 targetPosition;
+    NavMeshAgent zombieAgent;
+    ZombieAI zombieAI;
+
+    Node zombieBT;
+
+    private void Awake()
+    {
+        zombieAI = GetComponent<ZombieAI>();
+    }
+
+    void Start()
+    {
+        zombieAgent = GetComponent<NavMeshAgent>();
+
+        zombieBT = zombieAI.CreateBehaviorTree(this, mainTarget, _mask);
+    }
+
+    void Update()
+    {
+        zombieBT.Behave();
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GetDamaged(10f);
+        }
+    }
+
+    public void SetMainTarget(GameObject target)
+    {
+        mainTarget = target;
+    }
+
+    public GameObject GetMainTarget()
+    {
+        return mainTarget;
+    }
+
+    public void SetTargetPosition(Vector3 pos)
+    {
+        targetPosition = pos;
+    }
+
+    public bool TargetWithinDistance()
+    {
+        return Vector3.Distance(transform.position, mainTarget.transform.position) <= attackRange;
+    }
+
+    public void MoveToDestination()
+    {
+        Vector3 desiredLocation = new Vector3(targetPosition.x, transform.position.y, targetPosition.z);
+
+        if (Vector3.Distance(transform.position, desiredLocation) <= attackRange)
+        {
+            zombieAgent.isStopped = true;
+        }
+
+        zombieAgent.SetDestination(desiredLocation);
+    }
+
+
+
+
+
+
+
+    void GetDamaged(float amount)
+    {
+        healthPoint -= amount;
+    }
+
+    public float getHealthPoint()
+    {
+        return healthPoint;
+    }
+}
